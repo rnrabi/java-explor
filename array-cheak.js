5725964c64aba171ef09b1b254ea59c30418f430
rnrabi <rnrabi913@gmail.com>
2023-02-03 20:10:01 +0600

    that is ok

--- array-cheak.js
+++ array-cheak.js
@@ -25,29 +25,29 @@
 // 0- 100 টিকিট 100টাকা
 // 101 -200 টিকিট 90 টাকা 
 // 200+ টিাকিট 70 টাকা । একটি ফাংশন লিখ 250 বা অন্য কোনো সংখ্যার জন্য টিকিট কিনলে কত টাকা লাগবে । 
-const tikit100 = 100;
-const tikit200 = 90;
-const tikitUp200 =70;
-function tikit(quintity){
-    if(quintity <= 100){
-        let price = tikit100 * quintity;
-        return price;
-    }
-    else if(quintity > 100 && quintity <=200){
-        let price1 =tikit100 * 100;
-        let rest = quintity - 100;
-        let price2 = rest * tikit200;
-        let price = price1 + price2;
-        return price;
-    }
-    else{
-        let price1 = tikit100 *100;
-        let price2 = tikit200 * 100;
-        let rest = quintity - 200;
-        let price3 = rest *tikitUp200;
-        let price = price1 + price2 + price3;
-        return price;
-    }
-}
-let totalTaka = tikit(201);
-console.log(totalTaka);
+// const tikit100 = 100;
+// const tikit200 = 90;
+// const tikitUp200 =70;
+// function tikit(quintity){
+//     if(quintity <= 100){
+//         let price = tikit100 * quintity;
+//         return price;
+//     }
+//     else if(quintity > 100 && quintity <=200){
+//         let price1 =tikit100 * 100;
+//         let rest = quintity - 100;
+//         let price2 = rest * tikit200;
+//         let price = price1 + price2;
+//         return price;
+//     }
+//     else{
+//         let price1 = tikit100 *100;
+//         let price2 = tikit200 * 100;
+//         let rest = quintity - 200;
+//         let price3 = rest *tikitUp200;
+//         let price = price1 + price2 + price3;
+//         return price;
+//     }
+// }
+// let totalTaka = tikit(201);
+// console.log(totalTaka);
