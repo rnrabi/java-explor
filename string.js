7e597b28583fd23ea67e6c80cff7c42f9b84d096
rnrabi <rnrabi913@gmail.com>
2023-02-03 18:05:42 +0600

    fibonaci is complite

--- string.js
+++ string.js
@@ -54,9 +54,12 @@
 
 // console.log(result);
 // ফিবনাসি সংখ্যা প্রাক্টিস................
+// const fibo = [0, 1];
+// for(let i = 2; i<= 25; i++){
+//     fibo[i] = fibo[i-1] + fibo[i-2];
+// }
 
-
-
+// console.log(fibo);
 
 
 
